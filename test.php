64d1d9b7800b284b7b209f9489cb90d393da69cf
Petschko <peter-91@hotmail.de>
2016-09-15 16:55:53 +0200

    Updated example script

--- test.php
+++ test.php
@@ -1,20 +1,42 @@
 <?php
 
+// Require the MAin-Class (other classes will included by this file)
 require_once('includes' . DIRECTORY_SEPARATOR . 'DHLBusinessShipment.php');
 
 // Your customer and api credentials from/for DHL
-$credentials = array(
+$credentials = new DHL_Credentials();
+$credentials->setUser('geschaeftskunden_api');
+$credentials->setSignature('Dhl_ep_test1');
+$credentials->setEpk('5000000000');
+$credentials->setApiUser('');
+$credentials->setApiPassword('');
+
+/* Old one:
+ * $credentials = array(
 	'user' => 'geschaeftskunden_api',
 	'signature' => 'Dhl_ep_test1',
 	'ekp' => '5000000000',
 	'api_user'  => '',
 	'api_password'  => '',
 	'log' => true
-);
+);*/
 
 
-// your company info
-$info = array(
+// Your Company Info
+$info = new DHL_Company();
+$info->setCompanyName('Kindehochdrei GmbH');
+$info->setStreetName('Clayallee');
+$info->setStreetNumber('241');
+$info->setZip('14165');
+$info->setLocation('Berlin');
+$info->setCountry('Germany'); // doesn't matter if you write something upper case
+$info->setEmail('bestellung@kindhochdrei.de');
+$info->setPhone('01788338795');
+$info->setInternet('http://www.kindhochdrei.de');
+$info->setContactPerson('Nina Boeing');
+
+/* Old one:
+ * $info = array(
 	'company_name'    => 'Kindehochdrei GmbH',
 	'street_name'     => 'Clayallee',
 	'street_number'   => '241',
@@ -25,11 +47,21 @@ $info = array(
 	'phone'           => '01788338795',
 	'internet'        => 'http://www.kindhochdrei.de',
 	'contact_person'  => 'Nina Boeing'
-);
+);*/
 
 
-// receiver details
-$customer_details = array(
+// Receiver details
+$customer_details = new DHL_Receiver();
+$customer_details->setFirstName('Tobias');
+$customer_details->setLastName('Redmann');
+$customer_details->setCO(''); // Whatever
+$customer_details->setFullStreet('Hocksteinweg 11');
+$customer_details->setZip('14165');
+$customer_details->setLocation('Berlin');
+$customer_details->setCountry('Germany');
+
+/* Old one:
+* $customer_details = array(
 	'first_name'    => 'Tobias',
 	'last_name'     => 'Redmann',
 	'c/o'           => '',
@@ -38,7 +70,7 @@ $customer_details = array(
 	'country'       => 'germany',
 	'zip'           => '14165',
 	'city'          => 'Berlin'
-);
+);*/
 
 
 $dhl = new DHLBusinessShipment($credentials, $info);
@@ -48,4 +80,4 @@ $response = $dhl->createNationalShipment($customer_details);
 if($response !== false)
 	var_dump($response);
 else
-	var_dump($dhl->errors);
+	var_dump($dhl->getErrors());
