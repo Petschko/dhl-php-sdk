c57b2163901298779323973d22cf8fa007534dd8
Petschko <peter-91@hotmail.de>
2017-01-26 20:54:41 +0100

    Added Product-Type constance's and more Details to the DHL_ShipmentDetails-Class

--- includes/DHL_ShipmentDetails.php
+++ includes/DHL_ShipmentDetails.php
@@ -23,54 +23,131 @@ class DHL_ShipmentDetails {
 	 * DHL-Package-Type "Package"
 	 */
 	const PACKAGE = 'PK';
-	// todo add missing package types as const
+
+	/**
+	 * Product-Types
+	 */
+	const PRODUCT_TYPE_NATIONAL_PACKAGE = 'V01PAK';
+	const PRODUCT_TYPE_INTERNATIONAL_PACKAGE = 'V53WPAK';
+	const PRODUCT_TYPE_EUROPA_PACKAGE = 'V54EPAK';
+	const PRODUCT_TYPE_SAME_DAY_PACKAGE = 'V06PAK';
+	const PRODUCT_TYPE_SAME_DAY_MESSENGER = 'V06TG';
+	const PRODUCT_TYPE_WISH_TIME_MESSENGER = 'V06WZ';
+	const PRODUCT_TYPE_AUSTRIA_PACKAGE = 'V86PARCEL';
+	const PRODUCT_TYPE_AUSTRIA_INTERNATIONAL_PACKAGE = 'V82PARCEL';
+	const PRODUCT_TYPE_CONNECT_PACKAGE = 'V87PARCEL';
+
+	/**
+	 * @var string $product
+	 */
+	private $product = self::PRODUCT_TYPE_NATIONAL_PACKAGE;
+
+	/**
+	 * Min-Len: 14
+	 * Max-Len: 14
+	 *
+	 * @var string $accountNumber
+	 */
+	private $accountNumber;
+
+	/**
+	 * Note: Optional
+	 * Min-Len: -
+	 * Max-Len: 35
+	 *
+	 * @var string|null $customerReference
+	 */
+	private $customerReference = null;
+
+	/**
+	 * Note: ISO-Date-Format (YYYY-MM-DD)
+	 * Min-Len: 10
+	 * Max-Len: 10
+	 *
+	 * @var string $shipmentDate
+	 */
+	private $shipmentDate;
+
+	/**
+	 * Note: Optional
+	 * Min-Len: 14
+	 * Max-Len: 14
+	 *
+	 * @var string|null $returnAccountNumber
+	 */
+	private $returnAccountNumber = null;
+
+	/**
+	 * Note: Optional
+	 * Min-Len: -
+	 * Max-Len: 35
+	 *
+	 * @var string|null $returnReference
+	 */
+	private $returnReference = null;
 
 	/**
 	 * Weight of the Shipment-Object in KG
 	 *
-	 * @var int - Weight in KG
+	 * @var float $weight - Weight in KG
 	 */
-	private $weight = 5;
+	private $weight = 5.0;
 
 	/**
 	 * Length of the Shipment-Object in CM
 	 *
-	 * @var int - Length in CM
+	 * Note: Optional
+	 *
+	 * @var int|null $length - Length in CM
 	 */
-	private $length = 50;
+	private $length = null;
 
 	/**
 	 * Width of the Shipment-Object in CM
 	 *
-	 * @var int - Width in CM
+	 * Note: Optional
+	 *
+	 * @var int|null $width - Width in CM
 	 */
-	private $width = 50;
+	private $width = null;
 
 	/**
 	 * Height of the Shipment-Object in CM
 	 *
-	 * @var int - Height in CM
+	 * Note: Optional
+	 *
+	 * @var int|null $height - Height in CM
 	 */
-	private $height = 50;
+	private $height = null;
 
 	/**
 	 * Type of the Package
 	 *
-	 * @var string - Package-Type
+	 * Note: Optional
+	 *
+	 * @var string $packageType - Package-Type
 	 */
 	private $packageType = self::PACKAGE;
 
 	/**
 	 * DHL_ShipmentDetails constructor.
+	 *
+	 * @param string $accountNumber - Account-Number
 	 */
-	public function __construct() {
-		// VOID
+	public function __construct($accountNumber) {
+		$this->setAccountNumber($accountNumber);
 	}
 
 	/**
 	 * Clears the Memory
 	 */
 	public function __destruct() {
+		unset($this->product);
+		unset($this->accountNumber);
+		unset($this->customerReference);
+		unset($this->shipmentDate);
+		unset($this->returnAccountNumber);
+		unset($this->returnReference);
 		unset($this->weight);
 		unset($this->length);
 		unset($this->width);
@@ -79,56 +156,140 @@ class DHL_ShipmentDetails {
 	}
 
 	/**
-	 * @return int
+	 * @return string
+	 */
+	public function getProduct() {
+		return $this->product;
+	}
+
+	/**
+	 * @param string $product
+	 */
+	public function setProduct($product) {
+		$this->product = $product;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getAccountNumber() {
+		return $this->accountNumber;
+	}
+
+	/**
+	 * @param string $accountNumber
+	 */
+	private function setAccountNumber($accountNumber) {
+		$this->accountNumber = $accountNumber;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getCustomerReference() {
+		return $this->customerReference;
+	}
+
+	/**
+	 * @param null|string $customerReference
+	 */
+	public function setCustomerReference($customerReference) {
+		$this->customerReference = $customerReference;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getShipmentDate() {
+		return $this->shipmentDate;
+	}
+
+	/**
+	 * @param string $shipmentDate
+	 */
+	public function setShipmentDate($shipmentDate) {
+		$this->shipmentDate = $shipmentDate;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getReturnAccountNumber() {
+		return $this->returnAccountNumber;
+	}
+
+	/**
+	 * @param null|string $returnAccountNumber
+	 */
+	public function setReturnAccountNumber($returnAccountNumber) {
+		$this->returnAccountNumber = $returnAccountNumber;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getReturnReference() {
+		return $this->returnReference;
+	}
+
+	/**
+	 * @param null|string $returnReference
+	 */
+	public function setReturnReference($returnReference) {
+		$this->returnReference = $returnReference;
+	}
+
+	/**
+	 * @return float
 	 */
 	public function getWeight() {
 		return $this->weight;
 	}
 
 	/**
-	 * @param int $weight
+	 * @param float $weight
 	 */
 	public function setWeight($weight) {
 		$this->weight = $weight;
 	}
 
 	/**
-	 * @return int
+	 * @return int|null
 	 */
 	public function getLength() {
 		return $this->length;
 	}
 
 	/**
-	 * @param int $length
+	 * @param int|null $length
 	 */
 	public function setLength($length) {
 		$this->length = $length;
 	}
 
 	/**
-	 * @return int
+	 * @return int|null
 	 */
 	public function getWidth() {
 		return $this->width;
 	}
 
 	/**
-	 * @param int $width
+	 * @param int|null $width
 	 */
 	public function setWidth($width) {
 		$this->width = $width;
 	}
 
 	/**
-	 * @return int
+	 * @return int|null
 	 */
 	public function getHeight() {
 		return $this->height;
 	}
 
 	/**
-	 * @param int $height
+	 * @param int|null $height
 	 */
 	public function setHeight($height) {
 		$this->height = $height;
@@ -149,18 +310,31 @@ class DHL_ShipmentDetails {
 	}
 
 	/**
-	 * Returns an Array of this Object for DHL-Shipment Details
+	 * Returns an DHL-Class of this Object for DHL-Shipment Details
 	 *
-	 * @return array - Array with Values of this object
+	 * @return StdClass - ShipmentDetailsClass
 	 */
-	public function toDHLArray() {
-		$array = array();
-		$array['WeightInKG'] = $this->getWeight();
-		$array['LengthInCM'] = $this->getLength();
-		$array['WidthInCM'] = $this->getWidth();
-		$array['HeightInCM'] = $this->getHeight();
-		$array['PackageType'] = $this->getPackageType();
+	public function getShipmentDetailsClass_v2() {
+		$class = new StdClass;
 
-		return $array;
+		$class->product = $this->getProduct();
+		$class->accountNumber = $this->getAccountNumber();
+		if($this->getCustomerReference() !== null)
+			$class->customerReference = $this->getCustomerReference();
+		$class->shipmentDate = $this->getShipmentDate();
+		if($this->getReturnAccountNumber() !== null)
+			$class->returnShipmentAccountNumber = $this->getReturnAccountNumber();
+		if($this->getReturnReference() !== null)
+			$class->returnShipmentReference = $this->getReturnReference();
+		$class->ShipmentItem = new StdClass;
+		$class->ShipmentItem->weightInKG = $this->getWeight();
+		if($this->getLength() !== null)
+			$class->ShipmentItem->lengthInCM = $this->getLength();
+		if($this->getWidth() !== null)
+			$class->ShipmentItem->widthInCM = $this->getWidth();
+		if($this->getHeight() !== null)
+			$class->ShipmentItem->heightInCM = $this->getHeight();
+
+		return $class;
 	}
 }
