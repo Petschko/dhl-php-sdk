0996750d4bc94fc73ede62ad58f07232ba0c8ab2
Petschko <peter-91@hotmail.de>
2017-01-28 17:41:34 +0100

    Added ExportDocument-Class

---
+++ includes/DHL_ExportDocument.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: Peter Dragicevic [peter-91@hotmail.de]
+ * Authors-Website: http://petschko.org/
+ * Date: 26.01.2017
+ * Time: 21:05
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: -
+ */
+
+/**
+ * Class DHL_ExportDocument
+ */
+class DHL_ExportDocument {
+	// todo later
+}
