9f6be2c975b42f35930cc32d03db417242fcf156
Petschko <peter-91@hotmail.de>
2017-01-29 16:44:25 +0100

    Implemented create class method (v2)

--- includes/DHL_Receiver.php
+++ includes/DHL_Receiver.php
@@ -22,6 +22,7 @@ class DHL_Receiver extends DHL_SendPerson {
 	 */
 	public function getClass_v1() {
 		// TODO: Implement getClass_v1() method.
+		return new StdClass;
 	}
 
 	/**
@@ -31,7 +32,43 @@ class DHL_Receiver extends DHL_SendPerson {
 	 */
 	public function getClass_v2() {
 		$class = new StdClass;
-		// TODO: Implement getClass_v2() method.
+
+		$class->name1 = $this->getName();
+
+		// Communication
+		$class->Communication = new StdClass;
+		if($this->getPhone() !== null)
+			$class->Communication->phone = $this->getPhone();
+		if($this->getEmail() !== null)
+			$class->Communication->email = $this->getEmail();
+		if($this->getContactPerson() !== null)
+			$class->Communication->contactPerson = $this->getContactPerson();
+
+		// Address
+		$class->Address = new StdClass;
+		if($this->getName2() !== null)
+			$class->Address->name2 = $this->getName2();
+		if($this->$this->getName3() !== null)
+			$class->Address->name3 = $this->getName3();
+		$class->Address->streetName = $this->getStreetName();
+		$class->Address->streetNumber = $this->getStreetNumber();
+		if($this->getAddressAddition() !== null)
+			$class->Address->addressAddition = $this->getAddressAddition();
+		if($this->getDispatchingInfo() !== null)
+			$class->Address->dispatchingInformation = $this->getDispatchingInfo();
+		$class->Address->zip = $this->getZip();
+		$class->Address->city = $this->getLocation();
+
+		// Origin
+		if($this->getCountryISOCode() !== null) {
+			$class->Address->Origin = new StdClass;
+			if($this->getCountry() !== null)
+				$class->Address->Origin->country = $this->getCountry();
+			$class->Address->Origin->countryISOCode = $this->getCountryISOCode();
+			if($this->getState() !== null)
+				$class->Address->Origin->state = $this->getState();
+		}
+
 		return $class;
 	}
 }
