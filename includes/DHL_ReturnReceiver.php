f5f517a21cbcad397e039209e8ce1f26e09c8601
Petschko <peter-91@hotmail.de>
2017-01-29 16:57:38 +0100

    Added create class method (v2)

--- includes/DHL_ReturnReceiver.php
+++ includes/DHL_ReturnReceiver.php
@@ -7,7 +7,11 @@
  * Update: -
  * Version: 0.0.1
  *
- * Notes: -
+ * Notes: Contains the DHL_ReturnReceiver Class
+ */
+
+/**
+ * Class DHL_ReturnReceiver
  */
 class DHL_ReturnReceiver extends DHL_SendPerson {
 	/**
@@ -17,6 +21,7 @@ class DHL_ReturnReceiver extends DHL_SendPerson {
 	 */
 	public function getClass_v1() {
 		// TODO: Implement getClass_v1() method.
+		return new StdClass;
 	}
 
 	/**
@@ -25,6 +30,46 @@ class DHL_ReturnReceiver extends DHL_SendPerson {
 	 * @return StdClass - DHL-SendPerson-class
 	 */
 	public function getClass_v2() {
-		// TODO: Implement getClass_v2() method.
+		$class = new StdClass;
+
+		// Name
+		$class->Name = new StdClass;
+		$class->Name->name1 = $this->getName();
+		if($this->getName2() !== null)
+			$class->Name->name2 = $this->getName2();
+		if($this->getName3() !== null)
+			$class->Name->name3 = $this->getName3();
+
+		// Address
+		$class->Address = new StdClass;
+		$class->Address->streetName = $this->getStreetName();
+		$class->Address->streetNumber = $this->getStreetNumber();
+		if($this->getAddressAddition() !== null)
+			$class->Address->addressAddition = $this->getAddressAddition();
+		if($this->getDispatchingInfo() !== null)
+			$class->Address->dispatchingInformation = $this->getDispatchingInfo();
+		$class->Address->zip = $this->getZip();
+		$class->Address->city = $this->getLocation();
+
+		// Origin
+		if($this->getCountryISOCode() !== null) {
+			$class->Address->Origin = new StdClass;
+			if($this->getCountry() !== null)
+				$class->Address->Origin->country = $this->getCountry();
+			$class->Address->Origin->countryISOCode = $this->getCountryISOCode();
+			if($this->getState() !== null)
+				$class->Address->Origin->state = $this->getState();
+		}
+
+		// Communication
+		$class->Communication = new StdClass;
+		if($this->getPhone() !== null)
+			$class->Communication->phone = $this->getPhone();
+		if($this->getEmail() !== null)
+			$class->Communication->email = $this->getEmail();
+		if($this->getContactPerson() !== null)
+			$class->Communication->contactPerson = $this->getContactPerson();
+
+		return $class;
 	}
 }
