dd5966b991ac566f4616f2846faf70fef69ca288
Petschko <peter-91@hotmail.de>
2017-07-13 21:28:15 +0200

    Added a Loader-File for Non-Composer User

---
+++ includes/_nonComposerLoader.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Author: Peter Dragicevic [peter@petschko.org]
+ * Authors-Website: http://petschko.org/
+ * Date: 13.07.2017
+ * Time: 21:17
+ *
+ * Notes:	Require this File if you don't have Composer installed... It will load ALL Classes for this Lib
+ * 			But I suggest using Composer instead of this File - Get it here: https://getcomposer.org/
+ * 			------------------------------------
+ * 			IMPORTANT: Don't use this File, if you use Composer!
+ */
+
+// Set correct encoding
+mb_internal_encoding('UTF-8');
+
+// Get required classes
+// Abstract classes first
+require_once('Version.php');
+require_once('Address.php');
+require_once('SendPerson.php');
+
+// Now all other classes
+require_once('BankData.php');
+require_once('Credentials.php');
+require_once('ExportDocPosition.php');
+require_once('ExportDocument.php');
+require_once('IdentCheck.php');
+require_once('Product.php');
+require_once('ProductInfo.php');
+require_once('Receiver.php');
+	require_once('Filial.php');
+	require_once('PackStation.php');
+require_once('Response.php');
+require_once('ReturnReceiver.php');
+require_once('Sender.php');
+require_once('Service.php');
+require_once('ShipmentDetails.php');
+
+// Now the Base-Class
+require_once('BusinessShipment.php');
