1d8d4ba9f7e79418a82bdc2cf5e3f436c89683fd
Petschko <peter@petschko.org>
2018-08-13 17:30:45 +0200

    Added some more details to sender-example

--- test.php
+++ test.php
@@ -51,6 +51,9 @@ $sender->setZip('21037');
 $sender->setCity('Hamburg');
 $sender->setCountry('Germany');
 $sender->setCountryISOCode('DE');
+// $sender->setEmail('peter@petschko.org'); // These are super optional, it will printed on the label, can set under receiver as well
+// $sender->setPhone('015774121861');
+// $sender->setContactPerson('Anna Muster');
 
 // Set Receiver
 $receiver = new Receiver();
